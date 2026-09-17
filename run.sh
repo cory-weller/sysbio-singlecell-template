1a7bb32bfe8d8576410e08fc0e7651cffb752530
Cory Weller <cory.a.weller@gmail.com>
2026-09-17 11:09:58 -0400

    update template

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem-per-cpu 8G
+#SBATCH --time 24:00:00
+
+module purge
+module load snakemake/9.23.1
+
+snakemake ${@} \
+    --nolock \
+    --verbose \
+    --printshellcmds \
+    --profile settings/profile.v9+.yaml \
+    --configfile settings/config.yaml \
+    --conda-prefix envs \
+    --snakefile snakefile
